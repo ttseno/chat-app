3386b2a096c5a2e0dee1ba956d7cdc47e3913b0f
Talissa Tiemy Seno <tiemy.seno@gmail.com>
2022-06-15 00:24:19 -0300

    Add base socket server and client

---
+++ ChatBackend/ChatServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace ChatServer
+{
+    class Echo : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs message)
+        {
+            Console.WriteLine("Message received: " + message.Data);
+            Send(message.Data);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverUrl = "ws://localhost:5050";
+            var server = new WebSocketServer(serverUrl);
+            
+            server.AddWebSocketService<Echo>("/Echo");
+            
+            server.Start();
+            Console.WriteLine($"Starting server at {serverUrl}");
+            Console.ReadKey();
+            Console.WriteLine($"Stopping server at {serverUrl}");
+        }
+    }
+}
